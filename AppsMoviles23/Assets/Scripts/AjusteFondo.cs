ff889b665f267844b558af24e0064a8cbe2196a3
ScenicPenguin72 <ruzoemo97@hotmail.com>
2023-10-26 16:13:27 -0600

    otroreajuste

--- AppsMoviles23/Assets/Scripts/AjusteFondo.cs
+++ AppsMoviles23/Assets/Scripts/AjusteFondo.cs
@@ -2,28 +2,45 @@ using UnityEngine;
 
 public class AjusteFondo : MonoBehaviour
 {
+    public GameObject fondo; // Asigna el GameObject del fondo desde el Inspector
 
     private void Start()
     {
+        // Asegúrate de que el GameObject del fondo esté asignado en el Inspector.
+        if (fondo == null)
+        {
+            Debug.LogError("Asigna el GameObject del fondo en el Inspector.");
+            return;
+        }
+
         AjustarFondo();
     }
 
     private void AjustarFondo()
     {
-       float relacionDeAspectoPantalla = (float)Screen.width / Screen.height;
+        // Calcula la relación de aspecto de la pantalla (ancho / alto)
+        float relacionDeAspectoPantalla = (float)Screen.width / Screen.height;
 
-        // Calcula la relación de aspecto del fondo
-        float relacionDeAspectoFondo = 6.0f / 15.0f; // Ajusta esto a la relación de aspecto de tu fondo
-
-        // Calcula la escala en función de la relación de aspecto de la pantalla
         float escala = 1.0f;
-        if (relacionDeAspectoPantalla > relacionDeAspectoFondo)
+        if (relacionDeAspectoPantalla >= 0.405 && relacionDeAspectoPantalla < 0.5f)
         {
             // La pantalla es más ancha que el fondo
-            escala = relacionDeAspectoPantalla / relacionDeAspectoFondo;
+            escala = relacionDeAspectoPantalla / 0.5f;
+            
+            fondo.transform.localScale = new Vector3(escala*6f, escala*15f, 1.0f);
         }
 
-        // Aplica la escala al fondo
-        this.transform.localScale = new Vector3(escala*5, escala*12.5f, 1.0f);
+        if (relacionDeAspectoPantalla < 0.405f)
+        {
+            // La pantalla es más ancha que el fondo
+            escala = (Screen.height/(relacionDeAspectoPantalla*1000)) * 0.6f;
+            
+            fondo.transform.localScale = new Vector3(5, 12.5f, 1.0f);
+        }
+
+         if (relacionDeAspectoPantalla > 0.5f)
+        {
+            fondo.transform.localScale = new Vector3(6, 15, 1.0f);
+        }
     }
 }

158fb46c8833c60e20689ac3e26d7d138fcdc32e
ScenicPenguin72 <ruzoemo97@hotmail.com>
2023-10-31 22:56:54 -0600

    patron1

--- AppsMoviles23/Assets/Scripts/CrabMove.cs
+++ AppsMoviles23/Assets/Scripts/CrabMove.cs
@@ -5,30 +5,71 @@ using UnityEngine;
 public class CrabMove : MonoBehaviour
 {
     public GameObject background;
-
+    private GameObject player;
+    private Vector2 playerPosition;
     private float _bh, _bw;
     void Start()
     {
         _bh = background.transform.localScale.y;
         _bw = background.transform.localScale.x;
+        player = GameObject.FindGameObjectWithTag("Player");
+        playerPosition = player.transform.position;
     }
+
    public IEnumerator Move1()
     {
+        
         float time = 2f;
-        GameObject player = GameObject.FindWithTag("Player");
         float timer = time/3;
         while (timer < time)
         {
-            transform.position = Vector2.MoveTowards(transform.position, player.transform.position, 6 * Time.deltaTime);
+            transform.position = Vector2.MoveTowards(transform.position, playerPosition, 3*time * Time.deltaTime);
             timer += Time.deltaTime;
             yield return null;
         }
         timer = 0;
         while (timer < time)
         {
-            transform.position = Vector2.MoveTowards(transform.position, new Vector2(transform.position.x,_bh-_bh*2.2f), 4 * Time.deltaTime);
+            transform.position = Vector2.MoveTowards(transform.position, new Vector2(transform.position.x,_bh-_bh*2.2f), 2*time * Time.deltaTime);
             timer += Time.deltaTime;
             yield return null;
         }
     }
+
+    public IEnumerator SupportMove1()
+    {
+        float time = 2f;
+        float timer = 0;
+        while (timer < time)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, new Vector2(transform.position.x,-_bh*3.2f), 5*time * Time.deltaTime);
+            timer += Time.deltaTime;
+            yield return null;
+        }
+    }
+    public IEnumerator SupportMove2()
+    {
+        float time = 2f;
+        float timer = 0;
+        bool x = false;
+        while (timer < time && x == false)
+        {
+            timer += Time.deltaTime;
+            
+                transform.position = Vector2.MoveTowards(transform.position, new Vector2(transform.position.x,-_bh*3.2f), 5*time * Time.deltaTime);
+                x = (transform.position.y+0.06 >= playerPosition.y && transform.position.y-0.06 <= playerPosition.y) ? true : false;
+                yield return null;
+        }
+        int num = (playerPosition.x-transform.position.x>=0) ? 1 : -1;
+        while (timer < time*2 && x == true)
+        {
+            timer += Time.deltaTime;
+            
+                transform.position = Vector2.MoveTowards(transform.position, new Vector2(num*15, transform.position.y), 2*time * Time.deltaTime);
+            
+            
+            yield return null;
+        }
+    }
+
 }
--- AppsMoviles23/Assets/Scripts/Spawner.cs
+++ AppsMoviles23/Assets/Scripts/Spawner.cs
@@ -5,29 +5,42 @@ using System;
 
 public class Spawner : MonoBehaviour
 {
-    public GameObject Crab;
+    public GameObject Crab, background;
 
     private CrabMove crabMove;
     private float AC = 0.1f;
 
     private float[] time;
 
-
+    private float _bh, _bw;
+    void Start()
+    {
+        _bh = background.transform.localScale.y;
+        _bw = background.transform.localScale.x;
+    }
     void Update()
     {
         if(Time.time>AC)
         {
             System.Random random = new System.Random();
-            int randomNumber = random.Next(1, 7);
+            int randomNumber = random.Next(1, 4);
             int resultado = (randomNumber <= 5) ? 10 : 20;
             AC = 3 + Time.time + resultado;
-            StartCoroutine(P1(resultado));
+            if(randomNumber >= 5)
+            {
+                StartCoroutine(P1(resultado));
+            }
+            if(randomNumber < 5)
+            {
+                StartCoroutine(P1(resultado));
+                StartCoroutine(S1(resultado));
+            }
         }
     }
     public IEnumerator P1(float time)
     {
         float timer = 0.0f;
-        float cooldown = UnityEngine.Random.Range(0.5f, 1.2f); // Genera un valor aleatorio al principio
+        float cooldown = UnityEngine.Random.Range(0.5f, 1.2f); 
 
         while (timer < time)
         {
@@ -36,6 +49,32 @@ public class Spawner : MonoBehaviour
             GameObject Crab1 = Instantiate(Crab, transform.position, Quaternion.identity) as GameObject;
             CrabMove crabMove = Crab1.GetComponent<CrabMove>();
             crabMove.StartCoroutine(crabMove.Move1());
+
+            timer += cooldown;
+        }
+
+        // Asegurarse de que timer sea igual a time al final
+        if (timer < time)
+        {
+            yield return new WaitForSeconds(time - timer);
+        }
+    }
+    public IEnumerator S1(float time)
+    {
+        float timer = 0.0f;
+        float cooldown = UnityEngine.Random.Range(0.5f, 1.2f);
+        float place = UnityEngine.Random.Range(-_bw/2, _bw/2);
+
+        while (timer < time)
+        {
+            yield return new WaitForSeconds(cooldown);
+
+            GameObject Crab1 = Instantiate(Crab, new Vector2(place, transform.position.y), Quaternion.identity) as GameObject;
+            CrabMove crabMove = Crab1.GetComponent<CrabMove>();
+            crabMove.StartCoroutine(crabMove.SupportMove2());
+            GameObject Crab2 = Instantiate(Crab, new Vector2(-place, transform.position.y), Quaternion.identity) as GameObject;
+            CrabMove crabMove2 = Crab2.GetComponent<CrabMove>();
+            crabMove2.StartCoroutine(crabMove2.SupportMove2());
 
             timer += cooldown;
         }
